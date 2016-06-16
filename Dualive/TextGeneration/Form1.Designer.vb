f4237b6ac4174a006d236c6a5a76c90b6bff8dce
maxrchung <maxrchung@gmail.com>
2016-06-15 23:53:17 -0700

    Starting work on TextRender utility

--- Dualive/TextGeneration/Form1.Designer.vb
+++ Dualive/TextGeneration/Form1.Designer.vb
@@ -22,9 +22,110 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.NewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LoadToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FontToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RenderToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RichTextBox1 = New System.Windows.Forms.RichTextBox()
+        Me.FontDialog1 = New System.Windows.Forms.FontDialog()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NewToolStripMenuItem, Me.LoadToolStripMenuItem, Me.SaveToolStripMenuItem, Me.FontToolStripMenuItem, Me.RenderToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(784, 24)
+        Me.MenuStrip1.TabIndex = 0
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'NewToolStripMenuItem
+        '
+        Me.NewToolStripMenuItem.Name = "NewToolStripMenuItem"
+        Me.NewToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F1
+        Me.NewToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.NewToolStripMenuItem.Text = "New (F1)"
+        '
+        'LoadToolStripMenuItem
+        '
+        Me.LoadToolStripMenuItem.Name = "LoadToolStripMenuItem"
+        Me.LoadToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F2
+        Me.LoadToolStripMenuItem.Size = New System.Drawing.Size(68, 20)
+        Me.LoadToolStripMenuItem.Text = "Load (F2)"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F3
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.SaveToolStripMenuItem.Text = "Save (F3)"
+        '
+        'FontToolStripMenuItem
+        '
+        Me.FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        Me.FontToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F4
+        Me.FontToolStripMenuItem.Size = New System.Drawing.Size(66, 20)
+        Me.FontToolStripMenuItem.Text = "Font (F4)"
+        '
+        'RenderToolStripMenuItem
+        '
+        Me.RenderToolStripMenuItem.Name = "RenderToolStripMenuItem"
+        Me.RenderToolStripMenuItem.ShortcutKeys = System.Windows.Forms.Keys.F5
+        Me.RenderToolStripMenuItem.Size = New System.Drawing.Size(79, 20)
+        Me.RenderToolStripMenuItem.Text = "Render (F5)"
+        '
+        'RichTextBox1
+        '
+        Me.RichTextBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.RichTextBox1.BackColor = System.Drawing.SystemColors.MenuText
+        Me.RichTextBox1.Font = New System.Drawing.Font("Aroly", 99.74999!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox1.ForeColor = System.Drawing.SystemColors.Window
+        Me.RichTextBox1.Location = New System.Drawing.Point(0, 27)
+        Me.RichTextBox1.Name = "RichTextBox1"
+        Me.RichTextBox1.Size = New System.Drawing.Size(784, 534)
+        Me.RichTextBox1.TabIndex = 1
+        Me.RichTextBox1.Text = ""
+        '
+        'FontDialog1
+        '
+        Me.FontDialog1.ShowApply = True
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.FileName = "OpenFileDialog1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ClientSize = New System.Drawing.Size(784, 561)
+        Me.Controls.Add(Me.RichTextBox1)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.MinimumSize = New System.Drawing.Size(800, 600)
+        Me.Name = "Form1"
+        Me.Text = "TextRender"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents NewToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LoadToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SaveToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RichTextBox1 As System.Windows.Forms.RichTextBox
+    Friend WithEvents RenderToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents FontDialog1 As System.Windows.Forms.FontDialog
+    Friend WithEvents OpenFileDialog1 As System.Windows.Forms.OpenFileDialog
 
 End Class
--- Dualive/TextGeneration/Form1.vb
+++ Dualive/TextGeneration/Form1.vb
@@ -1,3 +1,35 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles Me.Load
+        FontDialog1.Font = RichTextBox1.Font
+    End Sub
+
+    Private Sub FontToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles FontToolStripMenuItem.Click
+        Dim prevFont As Font = RichTextBox1.Font
+        If FontDialog1.ShowDialog() <> Windows.Forms.DialogResult.Cancel Then
+            RichTextBox1.Font = FontDialog1.Font
+        Else
+            RichTextBox1.Font = prevFont
+        End If
+    End Sub
+
+    Private Sub LoadToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles LoadToolStripMenuItem.Click
+
+        If OpenFileDialog1.ShowDialog() <> Windows.Forms.DialogResult.Cancel Then
+            Dim fileName = OpenFileDialog1.FileName
+            RichTextBox1.Text = File.ReadAllText(fileName)
+        End If
+    End Sub
+
+    Private Sub RenderToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) _
+        Handles RenderToolStripMenuItem.Click
+
+    End Sub
+
+    Private Sub FontDialog_Apply(sender As Object, e As EventArgs) Handles FontDialog1.Apply
+        RichTextBox1.Font = FontDialog1.Font
+    End Sub
 End Class
